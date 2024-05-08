bd0ac5da2bdf892804a10fc62112bc8e10dd5468
Alan Vasquez <therankin2306@gmail.com>
2024-05-08 17:08:47 -0400

    docs: better comments

--- src/extension.ts
+++ src/extension.ts
@@ -47,11 +47,23 @@ function formatCode(code: string): string {
 
 	code = variables(code);
 
-	// Remove more than 2 empty lines
+	/**
+	 * F "<more than 2 new lines>"
+	 * T "<2 lines>"
+	 */
 	code = code.replaceAll(/\n{3,}/gm, '\n\n');
+
+	/**
+	 * F "}<any space>)"
+	 * T "})"
+	 */
 	code = code.replaceAll(/\}[\s\n]+\)/gm, '})');
 
-	// Trailing spaces
+	/**
+	 * Trailing spaces
+	 * F "<any extra space>"
+	 * T ""
+	 */
 	code = code.replaceAll(/[ \t]+$/gm, '');
 
 	// if final 2 lines are empty, remove one
@@ -62,9 +74,18 @@ function formatCode(code: string): string {
 }
 
 function semiColon(code: string): string {
+	/**
+	 * F ";<any variable>;"
+	 * T ";
+	 * <any variable>;"
+	 */
 	code = code.replaceAll(/;((?:(?!\/\/).)*?)(?=;)/gm, ';\n$1');
+
+	/**
+	 * F "<any variable><any space>;"
+	 * T "<any variable>;"
+	 */
 	code = code.replaceAll(/[\s\n]+;/gm, ';');
-	code = code.replaceAll(/;([ \w]+)/gm, ';\n$1');
 
 	// TODO: organize splitted words with /n
 	// code = code.replace(/(?<!;)\n *([\S]+);?/gm, ' $1');
@@ -72,16 +93,40 @@ function semiColon(code: string): string {
 }
 
 function keyOperator(code: string): string {
-	// Work with }
+	/**
+	 * F "} <any space>"
+	 * T "}
+	 * <any space>"
+	 */
 	code = code.replaceAll(/\}(?=[\S ]+)/gm, '}\n');
+
+	/**
+	 * From <anything>}
+	 * To <anything>;
+	 * }
+	 */
 	code = code.replaceAll(/(?:[\w;]+ *)\}/gm, ';\n}');
-	code = code.replaceAll(/\}\n *(?=.+\([\w ,]*\) *{)/gm, '}\n\n');
+	/**
+	 * F "}
+	 * <method name>(arg1,argN) {"
+	 * T "}
+	 *
+	 * "
+	 */
+	code = code.replaceAll(/\}\n *(?=.+\([\w ,]*\) *\{)/gm, '}\n\n');
 
 
-	// Work with {
+	/**
+	 * F "{<method name>(arg1,argN)"
+	 * T "{
+	 * <method name>(arg1,argN)"
+	 */
 	code = code.replaceAll(/\{(?=[\S\w ]+)/gm, '{\n');
 
-	// Add space after {
+	/**
+	 * F "<any space>{"
+	 * T " {"
+	 */
 	code = code.replaceAll(/[\s]*\{/gm, ' {');
 	return code;
 }
@@ -126,48 +171,90 @@ function indentation(code: string): string {
 }
 
 function conditions(code: string): string {
-	// Add space after if
+	/**
+	 * F "if       ("
+	 * T "if ("
+	 */
 	code = code.replaceAll(/if\s*\(/gm, 'if (');
 
+	/**
+	 * F "}        else {"
+	 * T "} else {"
+	 */
 	code = code.replaceAll(/\}[\s]*else \{/gm, '} else {');
 	return code;
 }
 
 function forLoop(code: string): string {
 
-	// Format for
+	/**
+	 * F "for*<any order and spaces>*{"
+	 * T "for (classic 'for' order) {"
+	*/
 	code = code.replaceAll(/for\s*\(\s*(\w+)\s+(\w+)\s*=\s*(\w+)?\s*;\s*(\w+)\s*([<>!=]+)\s*(\w+)\s*;\s*([\w-+=]+)\s*\)\s*{/gm, 'for ($1 $2=$3; $4$5$6; $7) {');
 
-	// Format foreach
+	/**
+	 * F "for*<any order and spaces>*{"
+	 * T "for (classic 'foreach' order) {"
+	 */
 	code = code.replaceAll(/for\s*\(\s*(\w+)\s+(\w)\s+:\s+(\w+)\s*\)\s*{/gm, 'for ($1 $2: $3) {');
 
+	// TODO: more complex 'for' loops with more args
 	return code;
 }
 
 function variables(code: string): string {
+	/**
+	 * F "
+	 * <method name>(args**) {"
+	 * T "
+	 *
+	 * <method name>(args**) {"
+	 */
 	code = code.replaceAll(/\n(?=.+\([\w ,]*\) *{)/gm, '\n\n');
+
+	// TODO: <T> type variables
 	return code;
 }
 
 function spaces(code: string): string {
+	/**
+	 * F "<any variable spaces>"
+	 * T "<type> <name>"
+	 */
 	code = code.replaceAll(/(?<![;\s]|\/\/.*|\*\/)[\s]{2,}/gm, ' ');
 	return code;
 }
 
 function methods(code: string): string {
-	// Remove spaces before (
+	/**
+	 * F "<method name> ("
+	 * T "<method name>("
+	 */
 	code = code.replaceAll(/\s+\(/gm, '(');
 
-	// Remove spaces after (
+	/**
+	 * F "<method name> ("
+	 * T "<method name>("
+	 */
 	code = code.replaceAll(/\(\s/gm, '(');
 
-	// Remove spaces before ,
+	/**
+	 * F "<method name>(arg1 , arg2)"
+	 * T "<method name>(arg1, arg2)"
+	 */
 	code = code.replaceAll(/\s+,/gm, ',');
 
-	// Add space after ,
+	/**
+	 * F "<method name>(arg1,    arg2)"
+	 * T "<method name>(arg1, arg2)"
+	 */
 	code = code.replaceAll(/,\s+/gm, ', ');
 
-	// Remove spaces before )
+	/**
+	 * F "<method name>(arg1, arg2 )"
+	 * T "<method name>(arg1, arg2)"
+	 */
 	code = code.replaceAll(/(?=.)\s+\)/gm, ')');
 	return code;
 }
